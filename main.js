0c2cd4ac757e091b857d4711bbc796114eba0d26
Lamson Le <ll217@u.washington.edu>
2015-08-26 19:46:26 -0700

    add button for each rider

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 $('#ast-btn').click(function() {
-
+  $('.rider').hide();
+  $('#astronaut').show();
 });
 $('#nja-btn').click(function() {
-
+  $('.rider').hide();
+  $('#ninja').show();
 });
 $('#cby-btn').click(function(){
-
+  $('.rider').hide();
+  $('#cowboy').show();
+});
+$('kny-btn').click(function() {
+  $('.rider').hide();
+  $('#kenny').show();
 });
