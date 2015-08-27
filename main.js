2e125e66320f6933a0fe6020378d8b141db44869
Lamson Le <ll217@u.washington.edu>
2015-08-26 20:00:30 -0700

    add transition

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 $('.rider').hide();
 
 $('#ast-btn').click(function() {
-  $('.rider').hide();
-  $('#astronaut').show();
+  $('.rider').hide("fast");
+  $('#astronaut').show("slow");
 });
 $('#nja-btn').click(function() {
-  $('.rider').hide();
-  $('#ninja').show();
+  $('.rider').hide("fast");
+  $('#ninja').show("slow");
 });
 $('#cby-btn').click(function(){
-  $('.rider').hide();
-  $('#cowboy').show();
+  $('.rider').hide("fast");
+  $('#cowboy').show("slow");
 });
-$('kny-btn').click(function() {
-  $('.rider').hide();
-  $('#kenny').show();
+$('#kny-btn').click(function() {
+  $('.rider').hide("fast");
+  $('#kenny').show("slow");
 });
